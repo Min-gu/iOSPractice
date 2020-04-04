5f4b9e96f0dbc87507a272b68321158cb43c1ac8
yepark <poisonF2@naver.com>
2020-04-04 19:47:21 +0900

    200404

---
+++ YeMemo/YeMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YeMemo
+//
+//  Created by 박영광 on 04/04/2020.
+//  Copyright © 2020 박영광. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
